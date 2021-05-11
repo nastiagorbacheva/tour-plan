414be30829103cd74fdff877bfe5ed0a950716a1
Anastasiya Gorbacheva <anastasya.bortnik@yandex.ru>
2021-05-12 04:15:34 +0700

    create: added swiper-slider in hotel section

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.hotel-slider__button--next',
+    prevEl: '.hotel-slider__button--prev',
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+});
