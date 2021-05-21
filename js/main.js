0330691cb12fa8a7beaddb5174799a3a300d2787
Anastasiya Gorbacheva <anastasya.bortnik@yandex.ru>
2021-05-22 05:18:18 +0700

    create: php code for forms fix: media (max-height: 576px)

--- js/main.js
+++ js/main.js
@@ -29,3 +29,4 @@ menuButton.addEventListener('click', function() {
   console.log("Клик по кнопке меню")
   document.querySelector(".navbar-bottom").classList.toggle("navbar-bottom--visible");
 });
+
---
+++ send.php
@@ -0,0 +1,59 @@
+<?php
+// Файлы phpmailer
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+// Переменные, которые отправляет пользователь
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+$email = array_key_exists('email', $_POST) ? $_POST['email'] : null;
+
+// Формирование самого письма
+$title = "Новое обращение Best Tour plan";
+$body = "
+<h2>Новое обращение</h2>
+<b>Имя:</b> $name<br>
+<b>Телефон:</b> $phone<br><br>
+<b> email:</b>$email<br>
+<b>Сообщение:</b><br>$message
+";
+
+// Настройки PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+    $mail->isSMTP();   
+    $mail->CharSet = "UTF-8";
+    $mail->SMTPAuth   = true;
+    // $mail->SMTPDebug = 2;
+    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+
+    // Настройки вашей почты
+    $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
+    $mail->Username   = 'bortniknasta58@gmail.com'; // Логин на почте
+    $mail->Password   = 'kptjwtokbloagsor'; // Пароль на почте
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+    $mail->setFrom('bortniknasta58@gmail.com', 'Анастасия Горбачёва'); // Адрес самой почты и имя отправителя
+
+    // Получатель письма
+    $mail->addAddress('anastasya.bortnik@yandex.ru');
+
+// Отправка сообщения
+$mail->isHTML(true);
+$mail->Subject = $title;
+$mail->Body = $body;    
+
+// Проверяем отравленность сообщения
+if ($mail->send()) {$result = "success";} 
+else {$result = "error";}
+
+} catch (Exception $e) {
+    $result = "error";
+    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
+
+// Отображение результата
+header('Location: thankyou.html');
+
