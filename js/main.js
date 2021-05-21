c7fab1df6d2196351830cc467c128389eb0f1a95
Anastasiya Gorbacheva <anastasya.bortnik@yandex.ru>
2021-05-21 09:07:19 +0700

    create: mobile menu

--- js/main.js
+++ js/main.js
@@ -24,4 +24,8 @@ const reviewSlider = new Swiper('.reviews-slider', {
   }
 });
 
-
+var menuButton = document.querySelector(".menu-button");
+menuButton.addEventListener('click', function() {
+  console.log("Клик по кнопке меню")
+  document.querySelector(".navbar-bottom").classList.toggle("navbar-bottom--visible");
+});
