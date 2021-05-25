cf7c3260aa6b6214a3d8e976493b5c5be1c6fc1a
Anastasiya Gorbacheva <anastasya.bortnik@yandex.ru>
2021-05-25 10:16:44 +0700

    create: added form validation

--- js/main.js
+++ js/main.js
@@ -1,43 +1,54 @@
 $(document).ready(function () {
-const hotelSlider = new Swiper('.hotel-slider', {
-  // Optional parameters
-  loop: true,
+  const hotelSlider = new Swiper('.hotel-slider', {
+    // Optional parameters
+    loop: true,
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.hotel-slider__button--next',
-    prevEl: '.hotel-slider__button--prev',
-  },
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
-});
+    // Navigation arrows
+    navigation: {
+      nextEl: '.hotel-slider__button--next',
+      prevEl: '.hotel-slider__button--prev',
+    },
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+  });
 
-const reviewSlider = new Swiper('.reviews-slider', {
-  // Optional parameters
-  loop: true,
+  const reviewSlider = new Swiper('.reviews-slider', {
+    // Optional parameters
+    loop: true,
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.reviews-slider__button--next',
-    prevEl: '.reviews-slider__button--prev',
-  }
-});
+    // Navigation arrows
+    navigation: {
+      nextEl: '.reviews-slider__button--next',
+      prevEl: '.reviews-slider__button--prev',
+    }
+  });
 
-var menuButton = $(".menu-button");
-menuButton.on('click', function() {
-  $(".navbar-bottom")
-  .toggleClass("navbar-bottom--visible");
-});
+var cardButton = $(".card__button");
+  cardButton.on('click', function () {
+    $(".navbar-bottom")
+      .toggleClass("navbar-bottom--visible");
+  });
+
+  cardButton.on("click", openModal);
+
+  var menuButton = $(".menu-button");
+  menuButton.on('click', function () {
+    $(".navbar-bottom")
+      .toggleClass("navbar-bottom--visible");
+  });
+
+
+
   var modalButton = $("[data-toggle=modal]");
   var closeModalButton = $(".modal__close");
   modalButton.on("click", openModal);
   closeModalButton.on("click", closeModal);
 
-  $(document).on('keyup', function(e) {
+  $(document).on('keyup', function (e) {
     if (e.key == "Escape") $(".modal__close").click();
-});
+  });
 
   function openModal() {
     var modalOverlay = $(".modal__overlay");
@@ -51,7 +62,34 @@ menuButton.on('click', function() {
     var modalDialog = $(".modal__dialog");
     modalOverlay.removeClass("modal__overlay--visible");
     modalDialog.removeClass("modal__dialog--visible");
-  };
-  
-});
-
+  }
+  // обработка форм
+  $(".form").each(function () {
+    $(this).validate({
+      errorClass: "invalid invalid__newsletter",
+      messages: {
+        name: {
+          required: "Please specify your name",
+          minlength: "Имя должно быть не короче 2 букв"
+        },
+        email: {
+          required: "We need your email address to contact you",
+        },
+        phone: {
+          required: "Please enter your phone number",
+        },
+      },
+    });
+  });
+  $(".newsletter__subscribe").validate({
+    errorLabelContainer: ".subscribe__error",
+    messages: {
+      email: {
+        required: "We need your email address to contact you",
+      },
+    }
+  })
+})
+$(document).ready(function () {
+  $('.phone_with_ddd').mask("+7 (999) 99-99-999");
+})
